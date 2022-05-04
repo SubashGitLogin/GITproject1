14958fd6ef54dc97a782211d425f9a287fdf2247
SubashGitLogin <subash007official@gmail.com>
2022-05-04 07:36:45 -0700

    Add some codes

--- src/org/test/Example.java
+++ src/org/test/Example.java
@@ -1,16 +1,19 @@
 package org.test;
 
 public class Example {
-	
-	int a=100;
-	int b=50;
-	
+private void empID(int a,int b) {
+	int c=a+b;
+	System.out.println("Adition :"+c);
+}	
 public static void main(String[] args) {
 	
 	System.out.println("Git Sucessful");
 	
 	System.out.println("Message Inserted");
 	System.out.println("Sucessful");
+	
+	Example e=new Example();
+	e.empID(25, 35);
 
    }
 }
